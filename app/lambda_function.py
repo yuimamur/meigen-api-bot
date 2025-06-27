92b11fb064d88fb768b6d1b593078be99f26789a
yuimamur <yuuya022221@gmail.com>
2025-06-27 16:18:23 +0900

    4th commit

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,21 +1,16 @@
-import requests
 import json
 import os
+import requests
 
 def lambda_handler(event, context):
-    # 名言を取得
-    res = requests.get("https://api.quotable.io/random")
-    data = res.json()
-    quote = f"{data['content']} — {data['author']}"
-
-    # Webhook URLを環境変数から取得
     webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+    message = {"text": "Hello, world!"}
 
-    # Slack に送る payload
-    payload = {"text": quote}
-
-    # Slack へ送信
-    response = requests.post(webhook_url, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
+    response = requests.post(
+        webhook_url,
+        data=json.dumps(message),
+        headers={"Content-Type": "application/json"}
+    )
 
     return {
         "statusCode": response.status_code,
