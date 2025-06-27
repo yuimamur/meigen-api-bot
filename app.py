d484c0f67be2292185ff9962737336046666851e
yuimamur <yuuya022221@gmail.com>
2025-06-28 00:21:16 +0900

    456

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import datetime
 import calendar
 
+time = datetime.datetime.now()
+str_time = time.strftime('%Y/%m/%d %H:%M:%S')
+
 def handler(event, context): 
-    return 'Hello from AWS Lambda using Python' +datetime.datetime.now() + '!'
+    return 'Hello from AWS Lambda using Python' + str_time  + '!'
 
